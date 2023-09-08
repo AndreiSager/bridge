2d6e95e29c4acc22dd4dbaeadcbffc923fbd8169
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-09-08 18:40:49 +0800

    Update Input.tsx

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,3 +1,36 @@
-export default function Input() {
-  return <div>Input</div>;
+"use client ";
+
+import { clsx } from "clsx";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  label: string;
+  id: string;
+  type?: string;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+  disabled?: boolean;
+}
+
+export default function Input({
+  label,
+  id,
+  type,
+  required,
+  register,
+  errors,
+  disabled,
+}: InputProps) {
+  return (
+    <div>
+      <label
+        className="block text-sm font-medium leading-6 text-gray-900"
+        htmlFor={id}
+      >
+        {label}
+      </label>
+      Input
+    </div>
+  );
 }
