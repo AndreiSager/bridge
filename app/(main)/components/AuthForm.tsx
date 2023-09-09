b0beafdac88adb7b1e7fb801e307c99ab4c36a0e
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-09-09 19:35:34 +0800

    Update AuthForm.tsx
    +It is done

--- app/(main)/components/AuthForm.tsx
+++ app/(main)/components/AuthForm.tsx
@@ -102,6 +102,16 @@ export default function AuthForm() {
             />
           </div>
         </div>
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>
+            {variant == "LOGIN"
+              ? "New to Messenger?"
+              : "Already have an account?"}
+          </div>
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant == "LOGIN" ? "Create an account" : "Login"}
+          </div>
+        </div>
       </div>
     </div>
   );
