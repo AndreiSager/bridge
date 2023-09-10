8a9440d086bdd69b17b69efd550f234b47db14b7
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-09-10 11:40:27 +0800

    Update Input.tsx

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -35,7 +35,7 @@ export default function Input({
           id={id}
           type={type}
           autoComplete={id}
-          disabled
+          disabled={disabled}
           {...register(id, { required })}
           className={clsx(
             "form-input block w-full rounded-md border-0 py-1.5 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6",
