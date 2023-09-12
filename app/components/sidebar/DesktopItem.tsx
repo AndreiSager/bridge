52275ce7e8a5b03ed64a55a201e3072aaad51881
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-09-12 18:23:50 +0800

    Update DesktopItem.tsx
    +Navigations have names for good seo

--- app/components/sidebar/DesktopItem.tsx
+++ app/components/sidebar/DesktopItem.tsx
@@ -15,7 +15,7 @@ interface DesktopItemProps {
 export default function DesktopItem({
   label,
   href,
-  icon,
+  icon: Icon,
   onClick,
   active,
 }: DesktopItemProps) {
@@ -27,8 +27,15 @@ export default function DesktopItem({
 
   return (
     <li onClick={handleClick}>
-      <Link href={href}>
-        <span>{label}</span>
+      <Link
+        href={href}
+        className={clsx(
+          "group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100",
+          active && "bg-gray-100 text-black"
+        )}
+      >
+        <Icon classname="h-6 aspect-square shrink-0" />
+        <span className="sr-only">{label}</span>
       </Link>
     </li>
   );
