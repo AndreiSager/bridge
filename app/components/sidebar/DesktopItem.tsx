5fadda6ce30ae6e05ce255a48ccaf8b709a7947e
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-10-29 19:56:51 +0800

    Update DesktopItem.tsx

--- app/components/sidebar/DesktopItem.tsx
+++ app/components/sidebar/DesktopItem.tsx
@@ -30,11 +30,11 @@ export default function DesktopItem({
       <Link
         href={href}
         className={clsx(
-          "group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100",
-          active && "bg-gray-100 text-black"
+          "group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-200 hover:blue-1 hover:bg-gray-100",
+          active && "bg-gray-100 text-blue-1"
         )}
       >
-        <Icon className="h-6 aspect-square shrink-0" />
+        <Icon className="h-[25px] w-[25px]" />
         <span className="sr-only">{label}</span>
       </Link>
     </li>
