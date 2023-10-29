454da8059cf6f37ee2c314ccd7fbe59f90edf33a
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-10-29 19:14:44 +0800

    Update tailwind.config.ts
    +Added palette colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {},
+    colors: {
+      "blue-1": "#024089",
+      "blue-2": "#004DA9",
+      "orange-1": "#FA841A",
+      "orange-2": "#FDA300",
+      "yellow-1": "#FFC619",
+      "online-green": "#5ED85C",
+    },
   },
   plugins: [
     require("@tailwindcss/forms")({
