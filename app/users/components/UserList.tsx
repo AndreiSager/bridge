7313ad0536c4a07938bd1da33f5ff78b54a0bd8d
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-10-31 19:26:58 +0800

    Update UserList.tsx

--- app/users/components/UserList.tsx
+++ app/users/components/UserList.tsx
@@ -10,15 +10,13 @@ interface UserListProps {
 
 export default function UserList({ items }: UserListProps) {
   return (
-    <aside className="flex flex-col overflow-hidden fixed inset-y-0 pb-0 lg:pb-0 lg:left-[60px] w-full lg:w-[300px] lg:block border-r-[1px] border-gray-200 gap-[10px]">
-      <div className="px-5">
-        <div className="flex flex-col">
-          <div className="text-2xl font-bold text-neutral-800 py-4">Bridge</div>
-        </div>
-        {items.map((item) => (
-          <UserBox key={item.id} data={item} />
-        ))}
+    <aside className="flex flex-col overflow-hidden fixed inset-y-0 pb-0 lg:pb-0 lg:left-[60px] w-full lg:w-[300px] lg:block border-r-[1px] border-gray-200 gap-[10px] h-full border-b-1 border-b-slate-200 px-[10px]">
+      <div className="flex justify-between items-center mb-2 pt-[23px]">
+        <div className="text-2xl font-bold text-neutral-800 ">Bridge</div>
       </div>
+      {items.map((item) => (
+        <UserBox key={item.id} data={item} />
+      ))}
     </aside>
   );
 }
